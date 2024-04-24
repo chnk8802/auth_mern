e81723fb626ae4b81644f183797599a212d39c6e
chnk8802 <chnk8802@gmail.com>
2024-04-24 18:00:14 +0530

    S-L-FP Done-2

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,31 +5,37 @@ export default function Header() {
   return (
     <AppBar position="static" elevation={4}>
       <Toolbar sx={{ backgroundColor: "#fff" }}>
-        <Typography
-          variant="h6"
-          color="primary"
-          component="div"
-          sx={{ flexGrow: 1 }}
-        >
-          Your App Name
-        </Typography>
-        <Grid spacing={2}>
-          <Button
-            color="primary"
-            variant="text"
-            disableElevation={true}
-            size="large"
-          >
-            Create an account
-          </Button>
-          <Button
-            color="primary"
-            variant="contained"
-            disableElevation={true}
-            size="large"
-          >
-            Login
-          </Button>
+        <Grid container spacing={2} sx={{ flexGrow: 1 }}>
+          <Grid item xs={8}>
+            <Typography
+              variant="h6"
+              color="primary"
+              component="div"
+              sx={{ flexGrow: 1 }}
+            >
+              Your App Name
+            </Typography>
+          </Grid>
+          <Grid item xs={2}>
+            <Button
+              color="primary"
+              variant="text"
+              disableElevation={true}
+              size="large"
+            >
+              Create an account
+            </Button>
+          </Grid>
+          <Grid item xs={2}>
+            <Button
+              color="primary"
+              variant="contained"
+              disableElevation={true}
+              size="large"
+            >
+              Login
+            </Button>
+          </Grid>
         </Grid>
       </Toolbar>
     </AppBar>
